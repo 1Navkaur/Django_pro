287c9a59d7eee289cc778e2e5ae604192662b4b7
navpreetk1 <navpreet.k.brudite@gmail.com>
2023-08-10 15:46:47 +0530

    classroom

--- project_1/app_1/urls.py
+++ project_1/app_1/urls.py
@@ -6,5 +6,13 @@ urlpatterns = [
     path('',main_view.main_view),
     path('Home/', main_view.main_menu),
     path('contact/', main_view.main_contact),
+     path('monday/', main_view.main_mon),
+      path('tuesday/', main_view.main_tue),
+       path('wednesday/', main_view.main_wed),
+        path('thursday/', main_view.main_thur),
+         path('friday/', main_view.main_fri), 
+          path('saturday/', main_view.main_sat),
+           path('sunday/', main_view.main_sun),
+           
     
 ]
--- project_1/app_1/views.py
+++ project_1/app_1/views.py
@@ -8,3 +8,18 @@ def main_contact(request):
     return render(request,'app_1/contact.html')
 def main_menu(request):
     return render(request,'app_1/index.html')
+def main_mon(request):
+    return render(request,'app_1/monday.html')
+def main_tue(request):
+    return render(request,'app_1/tuesday.html')
+def main_wed(request):
+    return render(request,'app_1/wednesday.html')
+def main_thur(request):
+    return render(request,'app_1/thursday.html')
+def main_fri(request):
+    return render(request,'app_1/friday.html')
+def main_sat(request):
+    return render(request,'app_1/saturday.html')
+def main_sun(request):
+    return render(request,'app_1/sunday.html')
+
